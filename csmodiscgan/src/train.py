4a88d53323f6d4d72319c541f4701ebc2395ce18
Randy J. Chase <randy.chase12@gmail.com>
2021-03-16 16:26:00 -0500

    changed training to fit with Colab disconnect

--- csmodiscgan/src/train.py
+++ csmodiscgan/src/train.py
@@ -355,9 +355,9 @@ def train_cs_modis_cgan_full(scenes_fn, run_name=None,GMIGAN=True,skinT=False,fu
 
 #     train_cs_modis_cgan(num_epochs=5, epoch=1, batch_size=32,
 #         **train_kwargs)
-    train_cs_modis_cgan(num_epochs=10, epoch=6, batch_size=64,
-        **train_kwargs)
-    train_cs_modis_cgan(num_epochs=10, epoch=16, batch_size=128,
+#     train_cs_modis_cgan(num_epochs=10, epoch=6, batch_size=64,
+#         **train_kwargs)
+    train_cs_modis_cgan(num_epochs=2, epoch=24, batch_size=128,
         **train_kwargs)
     train_cs_modis_cgan(num_epochs=20, epoch=26, batch_size=256,
         **train_kwargs)
